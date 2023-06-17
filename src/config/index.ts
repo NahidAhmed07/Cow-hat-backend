61116487407876dfd8bbfb77b13e7a5987efd97b
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2023-06-17 11:41:27 +0600

    [Done] eslint and prettier and vscode setting setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,12 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({
+  path: path.join(process.cwd(), '.env'),
+});
 
 export const config = {
   port: process.env.PORT || 5000,
   db_uri: process.env.DATABASE_URI,
-  env: process.env.NODE_ENV || "development",
+  env: process.env.NODE_ENV || 'development',
 };
